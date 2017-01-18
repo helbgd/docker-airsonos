10152c0f8e1f19a468d00084363f1c3be94b2a3b
Michael Riedel <michael.r141@gmail.com>
2017-01-18 11:08:15 +0100

    Corrected permission
    Problem with the global npm installation

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 #rm /usr/bin/chfn
 #ln -s -f /bin/true /usr/bin/chfn
 
-curl -sL https://deb.nodesource.com/setup | sudo bash -
+curl -sL https://deb.nodesource.com/setup_7.x | bash -
 
 apt-get -q update && apt-get install -qy \
   build-essential \
@@ -20,10 +20,10 @@ apt-get -q update && apt-get install -qy \
 #sed -i -e "s#\#enable-dbus=yes#enable-dbus=false#g" /etc/avahi/avahi-daemon.conf
 #sed -i -e "s/^rlimit-nproc/#rlimit-nproc/g" /etc/avahi/avahi-daemon.conf
 
-npm install -g git://github.com/Kolky/nodetunes.git#master
+npm install -g git://github.com/Kolky/nodetunes.git#master --unsafe-perm
 
 cd /var/tmp/
 git clone https://github.com/lsmith77/airsonos
 cd airsonos
 npm install babel
-npm install -g
+npm install -g --unsafe-perm
